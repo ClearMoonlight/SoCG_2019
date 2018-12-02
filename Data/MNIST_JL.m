942de4298210a839fc2709c433db3faccfe9f271
ClearMoonlight <yemingqu@msu.edu>
2018-12-02 18:56:54 -0500

    Data

---
+++ Data/MNIST_JL.m
@@ -0,0 +1,24 @@
+clear
+clc
+title=[4,8,16];
+orRt='/home/yemingqu/Mingquan/SoCG_2019/Data/MNIST/Gray/';
+deRt='/home/yemingqu/Mingquan/SoCG_2019/Data/MNIST/JL/';
+files=dir(orRt);
+
+Num=numel(files);
+num=numel(title);
+for candn=1:num
+    for i=3:Num
+        for j=5:5:30
+            load([orRt,files(i).name,'/P_',num2str(j),'.mat']);
+            load([orRt,files(i).name,'/label_',num2str(j),'.mat']);
+            d=size(P,1);
+            k=ceil(1/title(candn)*d);
+            P=randn(k,d)*P;
+            save([deRt,'1-',num2str(title(candn)),'/',...
+                files(i).name,'/P_',num2str(j),'.mat'],'P');
+            save([deRt,'1-',num2str(title(candn)),'/',...
+                files(i).name,'/label_',num2str(j),'.mat'],'label');
+        end
+    end
+end
